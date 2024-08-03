8cfe4f3f07b4bf725dec9fbe90361c69362f934c
ctudel <christudela@u.boisestate.edu>
2024-08-03 16:33:27 -0600

    Send button achieved

--- app/ui/textbox.tsx
+++ app/ui/textbox.tsx
@@ -1,7 +1,10 @@
 export default function Textbox() {
+    
+
     return (
-        <div className="p-3 flex flex-row justify-center font-medium">
-            <input type="text" className="p-3 border-2 rounded border-black text-black" name="user-text" placeholder="What're you gunna do now?"/> 
+        <div className="p-3 flex flex-col items-center justify-center gap-3 font-medium">
+            <input type="text" className="p-3 border-2 border-solid border-black rounded text-black" name="user-text" placeholder="What're you gunna do now?"/> 
+            <button className="px-3 py-0.5 text-white font-bold bg-black rounded hover:text-black hover:bg-slate-300 dark:text-black dark:bg-white dark:hover:text-white dark:hover:bg-black" type="submit">Send -></button>
         </div>
     )
 }
