c1ad680a9b8f334d9de10f79c62f06e87da2c5fc
Chris Tudela <christudela@u.boisestate.edu>
2024-08-04 22:34:21 -0600

    fixed textbox bg

--- app/ui/textbox.tsx
+++ app/ui/textbox.tsx
@@ -14,12 +14,12 @@ export default function Textbox() {
 
 
     function handleSubmit(e) {
-        e.preventDefault();
-        console.log("Form submitted with input: "+userText);
+        /*e.preventDefault();
+        console.log("Form submitted with input: "+userText);*/
     } 
 
     return (
-        <div className="p-4 flex flex-col items-center justify-center gap-3 font-medium">
+        <div className="flex flex-col items-center justify-center bg-white gap-3 font-medium">
             <form className="p-3 border-2 border-solid border-black rounded" onSubmit={handleSubmit}>
                 <input type="text" onChange={handleInput} className="p-3 outline-0 text-black" name="user-text" placeholder="What're you gunna do now?"/> 
                 <button className="px-3 py-0.5 text-white font-bold bg-black rounded hover:text-black hover:bg-slate-300 dark:text-black dark:bg-white dark:hover:text-white dark:hover:bg-black" type="submit">-></button>
