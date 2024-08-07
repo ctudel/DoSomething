dbc102beca98bfc0c365f3df1faf160be863510a
Chris Tudela <christudela@u.boisestate.edu>
2024-08-06 21:00:18 -0600

    navigation to task page draft

--- app/ui/textbox.tsx
+++ app/ui/textbox.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Textbox() { 
     const [userText, setUserText] = useState('');
+    const router = useRouter();
 
     /* Called once user has stopped typing for 300ms */
     const handleInput = useDebouncedCallback( (e: any) => {
@@ -17,11 +19,12 @@ export default function Textbox() {
         e.preventDefault();
         console.log("Form submitted with input: "+userText);
         console.log(getRandomTask("Hello"));
+        router.push("/task");
     } 
 
     return (
         <div className="flex flex-col items-center justify-center gap-3">
-            <form autocomplete="off" className="p-3 bg-white border-5 border-solid border-black rounded font-medium" onSubmit={handleSubmit}>
+            <form autoComplete="off" className="p-3 bg-white border-5 border-solid border-black rounded font-medium" onSubmit={handleSubmit} target="_blank">
                 <input type="text" onChange={handleInput} className="p-3 outline-0 text-black" name="user-text" placeholder="What're you gunna do now?"/> 
                 <button className="px-3 py-0.5 text-white font-bold bg-black border-2 border-solid rounded border-black hover:text-black hover:bg-white" type="submit">{"->"}</button>
             </form>
