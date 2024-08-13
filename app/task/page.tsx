241acd7806d0fdc402c2ea3525eda3296edc08cd
ctudel <christudela@u.boisestate.edu>
2024-08-12 21:54:41 -0600

    added styling

--- app/task/page.tsx
+++ app/task/page.tsx
@@ -3,7 +3,7 @@ import AltTask from '@/app/ui/alt-task';
 export default function Page() {
     return (
         <>
-            <div className="flex w-screen h-screen justify-center items-center">
+            <div className="p-3 flex w-screen h-screen justify-center items-center text-center">
                 <AltTask/>
             </div>
         </>
