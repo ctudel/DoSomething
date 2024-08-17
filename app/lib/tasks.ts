0656eaf4fd4d81cb9ac13fcf06a52a76e4333015
ctudel <christudela@u.boisestate.edu>
2024-08-16 21:42:15 -0600

    added task suggestions

--- app/lib/tasks.ts
+++ app/lib/tasks.ts
@@ -1,20 +1,27 @@
 export const habits: string[] = [
-    'phone',
-    'lay',
-    'bed',
-    'watch',
-    'nap',
-    'sleep',
-    'chill',
-    'nothing',
-    "i don't know",
-    'not sure',
+  'phone',
+  'lay',
+  'bed',
+  'watch',
+  'nap',
+  'sleep',
+  'chill',
+  'nothing',
+  "i don't know",
+  'not sure',
 ];
 
 export const altTasks: string[] = [
-    "Let's do something else!",
-    "Take a walk, see if that changes!",
-    "Let's explore other options while grabbing a snack!",
+  "Let's do something else!",
+  "Take a walk, see if that changes!",
+  "Let's explore other options while grabbing a snack!",
+  "Write down your thoughts on the week instead with a short journaling exercise.",
+  "Have a favorite book or podcast? Take a few moments to read a couple pages or listen for few minutes.",
+  "Grab something to drink while thinking of other options. I'm off to make my coffee as well!",
+  "Start a quick breathing exercise as we clear our minds and explore other options.",
+  "Let's do some yoga to put our minds and bodies at ease, as we think of better options.",
+  "Have a hobby or favorite activity? Let's do one of those!",
+  "Do some light stretching, see if you change your mind.",
 ]
 
 
