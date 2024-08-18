358550f50f0bfc621e7f5718171b1c46dafce8a5
ctudel <christudela@u.boisestate.edu>
2024-08-18 13:16:17 -0600

    drafted simple scrum db api

---
+++ pages/api/db.ts
@@ -0,0 +1,65 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Database from 'better-sqlite3';
+
+type ResponseData = {
+  message: string
+}
+
+// Import and initialize database
+const db = new Database('somethingtodo_db.sqlite', { verbose: console.log });
+db.exec(`CREATE TABLE IF NOT EXISTS 
+todos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  task TEXT NOT NULL
+)`);
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+
+  if (req.method === 'POST') {
+    addTodo(req, res);
+  }
+
+  else if (req.method === 'GET') {
+    getAllTodos(req, res);
+  }
+
+  else { res.status(404).json({ message: `${req.method} is not supported` }); }
+}
+
+
+
+/* Posts form data to todo table 
+ *   Creates and adds a new task
+ */
+let addTodo = (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) => {
+  // Form info
+  const { task } = req.body;
+  const insertData = db.prepare('INSERT INTO todos (task) VALUES (?)')
+  const data = insertData.run(task);
+
+  console.log('Changes:', data.changes);
+  console.log('Last Insert Row ID', data.lastInsertRowid);
+  console.log(data);
+
+  res.status(201).json({ message: 'SUCCESSFULLY inserted task into todo table' });
+}
+
+
+
+/* Gets all tasks from todo table */
+let getAllTodos = (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) => {
+  // All Tasks
+  const getData = db.prepare('SELECT * FROM todos');
+  const data = getData.all();
+
+  res.status(200).json(data);
+}
